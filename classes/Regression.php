90119d054ca4ff87760676727da2458051cbd8d4
Arancaytar <arancaytar@ermarian.net>
2010-08-07 12:18:35 +0000

    Properly implemented polynomial regression calculation (which was missing a matrix-inversion function).

--- classes/Regression.php
+++ classes/Regression.php
@@ -18,52 +18,106 @@ function linear_regression($points=array())
   return array($offset, $slope); 
 }
 
-function polynomial_regression($points = array(), $degree)
-{
-  $matrix = array();
-  foreach ($points as $i=>$point)
-  {
+function polynomial_regression($points = array(), $degree) {
+  $mX = array();
+  foreach ($points as $i => $point) {
     $x = 1;
     for ($j = 0; $j <= $degree; $j++) {
-      $matrix[$i][$j] = $x;
+      $mX[$i][$j] = $x;
       $x *= $point['x'];
     }
-    $vector[$i] = $point['y'];
+    $mY[$i] = array($point['y']);
   }
-  $b = matrix_transpose($matrix);
-  $product = matrix_multiply($b, $matrix);
-      if (DEBUG) var_dump($product);
-  $y = matrix_multiply($b, $vector);
-  $coefficients = matrix_multiply($product, $y);
+  $left = matrix_invert(matrix_multiply(matrix_transpose($mX), $mX));
+  $right = matrix_multiply(matrix_transpose($mX), $mY);
+  $coefficients = matrix_multiply($left, $right);
+  foreach ($coefficients as &$c) $c = $c[0];
+  return $coefficients;
 }
 
-function matrix_transpose($a)
-{
-  $b = array();
-  for ($i = 0; $i<count($a); $i++)
-  {
-    for ($j = 0; $j<count($a[$i]); $j++)
-    {
-      $b[$j][$i] = $a[$i][$j];
+/**
+ * Transpose a matrix.
+ */
+function matrix_transpose($matrix) {
+  $transposed = array();
+  foreach ($matrix as $i => $row) {
+    foreach ($row as $j => $cell) {
+      $transposed[$j][$i] = $cell;
     }
   }
-  return $b;
+  foreach ($transposed as $row) ksort($row);
+  ksort($transposed);
+  return $transposed;
 }
 
-function matrix_multiply($a, $b)
-{
-  if (count($a[0]) != count($b)) return false;
-  for ($i = 0; $i <= count($a); $i++)
-  {
+/**
+ * Multiply two matrices.
+ */
+function matrix_multiply($left, $right) {
+  // Match left cleft to right height.
+  if (count($left[0]) != count($right)) return FALSE;
+  for ($i = 0; $i < count($left); $i++) {
     $product[$i] = array();
-    for ($j = 0; $j <= count($b[0]); $j++)
-    {
+    for ($j = 0; $j < count($right[0]); $j++) {
       $product[$i][$j] = 0;
-      for ($k = 0; $k < count($a[0]); $k++)
-      {
-        $product[$i][$j]+= $a[$i][$k]*$b[$k][$j];
+      for ($k = 0; $k < count($left[0]); $k++) {
+        $product[$i][$j] += $left[$i][$k] * $right[$k][$j];
       }
     }
   }
   return $product;
 }
+
+function mprint($matrix) {
+  for ($i = 0; $i < count($matrix); $i++) print implode(", ", $matrix[$i]) . "\n";
+  print "\n\n";
+}
+
+/**
+ * Invert a matrix.
+ */
+function matrix_invert($matrix) {
+  // Append the identity to the right.
+  for ($i = 0; $i < count($matrix); $i++) {
+    for ($j = 0; $j < $i; $j++) $matrix[$i][] = 0;
+    $matrix[$i][] = 1;
+    for ($j = $i+1; $j < count($matrix); $j++) $matrix[$i][] = 0;
+  }
+  
+  // Reduce to echelon form.
+  for ($i = 0; $i < count($matrix); $i++) {
+    // If pivot is 0, switch rows.
+    if ($matrix[$i][$i] == 0) {
+      for ($j = $i+1; $j < count($matrix); $j++) {
+        if ($matrix[$j][$i] != 0) {
+          $r = $matrix[$j];
+          $matrix[$j] = $matrix[$i];
+          $matrix[$i] = $r;
+          break;
+        }
+      }
+      if ($matrix[$i][$i] == 0) return FALSE; // Singular.
+    }
+    
+    // Normalize column to 1 in this row.
+    $pivot = $matrix[$i][$i];
+    for ($j = 0; $j < count($matrix[$i]); $j++) {
+      $matrix[$i][$j] /= $pivot;
+    }
+    
+    // Reduce the rest of the column to 0.
+    for ($j = 0; $j < count($matrix); $j++) {
+      if ($j == $i) continue;
+      $factor = $matrix[$j][$i];
+      for ($k = 0; $k < count($matrix[$j]); $k++) {
+        $matrix[$j][$k] -= $factor * $matrix[$i][$k];
+      }
+    }
+  }
+  // Return the right half.
+  for ($i = 0; $i < count($matrix); $i++) {
+    $matrix[$i] = array_slice($matrix[$i], count($matrix));
+  }
+  return $matrix;
+}
+
